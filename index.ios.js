77dc325b987acef198c00d6e88eebee94444475c
pitaz <peter.odekwo@gmail.com>
2019-07-24 16:21:01 +0100

    add album list

--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View, Text, AppRegistry } from 'react-native';
 import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => {
   return (
     <View>
       <Header headerText={ 'Albums' } />
+      <AlbumList />
     </View>
   )
 };
